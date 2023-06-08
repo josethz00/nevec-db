f455197aaeea9644f1296a1b1fd2d84479462bd2
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-06-08 19:18:14 -0300

    feat: nevecdb class

--- src/main/scala/core/NevecDB.scala
+++ src/main/scala/core/NevecDB.scala
@@ -1,7 +1,16 @@
 package core
 import scala.collection.mutable
+import java.nio.file._
 
 class NevecDB(dbName: String, dimensions: Int) {
     val dbName: String = dbName
     val dimensions: String = dimensions
+
+    val path = Paths.get(s"$dbName.json")
+    if (!Files.exists(path)) {
+        Files.createFile(path)
+        print("Database created succesfully")
+    } else {
+        print("A database with this name already exists")
+    }
 }
