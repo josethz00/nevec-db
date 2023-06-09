321ef5b665a9ed40360937733083cf413f8de847
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-06-08 22:59:31 -0300

    feat: create index file

--- src/main/scala/core/NevecDB.scala
+++ src/main/scala/core/NevecDB.scala
@@ -1,19 +1,21 @@
 package core
 
 import scala.collection.mutable
-import java.nio.file._
 import annoy4s._
+import utils.FileUtil
 
 class NevecDB(name: String, dim: Int) {
     val dbName: String = name
     val dimensions: Int = dim
-    val index = Annoy.create[Int]("./input_vectors", 10, outputDir = "./annoy_result/", Angular)
 
-    val path = Paths.get(s"$dbName.json")
-    if (!Files.exists(path)) {
-        Files.createFile(path)
-        print("Database created succesfully")
-    } else {
-        print("A database with this name already exists")
-    }
+    val isDbFileCreated = FileUtil.createFile(s"$dbName.json")
+
+    val index: Option[Annoy[Int]] = 
+        if (isDbFileCreated) {
+            FileUtil.createFile(s"$dbName-input-vectors.ann")
+            Some(Annoy.create[Int](s"$dbName-input-vectors.ann", 10, outputDir = s"./$dbName-results/", Angular))
+        } else {
+            None
+        }
+    print("aaaaaaaaaaaaa")
 }
--- src/main/scala/utils/FileUtil.scala
+++ src/main/scala/utils/FileUtil.scala
@@ -4,14 +4,14 @@ import java.nio.file._
 
 object FileUtil {
   def createFile(filename: String): Boolean = {
-        val path = Paths.get(filename)
-        if (!Files.exists(path)) {
-            Files.createFile(path)
-            print("File created succesfully")
-            return true
-        } else {
-            print("A file with this name already exists")
-            return false
-        }
+    val path = Paths.get(filename)
+    if (!Files.exists(path)) {
+        Files.createFile(path)
+        println("File created succesfully")
+        return true
+    } else {
+        println("A file with this name already exists")
+        return false
+    }
   }
 }
