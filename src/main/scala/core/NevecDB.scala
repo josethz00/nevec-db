15b85396154370958c37d1c69eb009727ac17196
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-06-08 22:35:02 -0300

    refactor(fileUtil): return boolean

--- src/main/scala/core/NevecDB.scala
+++ src/main/scala/core/NevecDB.scala
@@ -1,10 +1,13 @@
 package core
+
 import scala.collection.mutable
 import java.nio.file._
+import annoy4s._
 
 class NevecDB(name: String, dim: Int) {
     val dbName: String = name
     val dimensions: Int = dim
+    val index = Annoy.create[Int]("./input_vectors", 10, outputDir = "./annoy_result/", Angular)
 
     val path = Paths.get(s"$dbName.json")
     if (!Files.exists(path)) {
--- src/main/scala/utils/FileUtil.scala
+++ src/main/scala/utils/FileUtil.scala
@@ -3,13 +3,15 @@ package utils
 import java.nio.file._
 
 object FileUtil {
-  def createFile(filename: String): Unit = {
+  def createFile(filename: String): Boolean = {
         val path = Paths.get(s"$filename.json")
         if (!Files.exists(path)) {
             Files.createFile(path)
             print("File created succesfully")
+            return true
         } else {
             print("A file with this name already exists")
+            return false
         }
   }
 }
