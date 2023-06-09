f684ffcf52fb15bd0171c30f42fb093f7be5e040
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-06-08 22:31:52 -0300

    feat(utils): create Object to handle files

--- src/main/scala/utils/FileUtil.scala
+++ src/main/scala/utils/FileUtil.scala
@@ -1,5 +1,15 @@
 package utils
 
+import java.nio.file._
+
 object FileUtil {
-  
+  def createFile(filename: String): Unit = {
+        val path = Paths.get(s"$filename.json")
+        if (!Files.exists(path)) {
+            Files.createFile(path)
+            print("File created succesfully")
+        } else {
+            print("A file with this name already exists")
+        }
+  }
 }
